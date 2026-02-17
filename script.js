1bd2e325cc13a9764e3b92ce42f3b0faf3609cd1
Keshav-Thathai <thataikeshav873@gmail.com>
2026-02-17 19:15:27 +0530

    Refactor number guessing game with improved logic
    Updated the number guessing game logic to include random number generation and improved user feedback messages.

--- script.js
+++ script.js
@@ -1,15 +1,28 @@
-var secretNumber=6;
-function checkNumber()
-{
-    var userGuess=document.getElementById("guess").value;
-    if(userGuess==secretNumber)
-    {
-        document.getElementById("result").innerText="You Win";
-
+let secretNumber=(Math.random()*100)+1;
+let tries=0;
+function checkGuess(){
+    let guess=Number(document.getElementById("guess").value);
+    let message=document.getElementById("message");
+    let attemptDisplay=document.getElementById("attempts");
+    tries++;
+    let difference=Math.abs(secretNumber-guess);
+    if (guess === secretNumber) {
+        message.textContent = "🎉 Correct Guess!";
     }
-    else{
-        document.getElementById("result").innerText="You lose";
-
-
+    else if (difference <= 6) {
+        message.textContent = "🔥 Too close!";
     }
+    else if (guess > secretNumber) {
+        message.textContent = "📈 Too high!";
+    }
+    else {
+        message.textContent = "📉 Too low!";
+    }
+
+    attemptDisplay.textContent = "Tries: " + tries;
 }
+
+    
+
+
+
